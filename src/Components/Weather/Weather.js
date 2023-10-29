dce5ea8901f3772742975b47e161b7094620fdf9
AndreyGOIT <ae.borga@gmail.com>
2023-10-29 14:07:11 +0200

    base fetch works

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,24 +1,22 @@
 import React, { useEffect } from "react";
 
 const Weather = () => {
-  //   const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
+  const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
 
   useEffect(() => {
+    console.log("API Key:", apiKey); // Убедитесь, что apiKey считывается правильно
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=60.17&lon=24.94&appid=7499e9f0cfd6cb862a73f272fd17492b
-`
+      `https://api.openweathermap.org/data/2.5/weather?lat=60.17&lon=24.94&appid=${apiKey}`
     )
       .then((response) => response.json())
       .then((data) => {
-        // Обработка полученных данных
-        console.log(data);
+        console.log("API Response:", data); // Проверьте ответ от API
       })
       .catch((error) => {
-        // Обработка ошибок
-        console.error(error);
+        console.error("API Error:", error); // Проверьте ошибки при сетевом запросе
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); // Пустой массив вторым аргументом означает, что useEffect будет выполняться только после первого рендера
+  }, []);
 
   return <div>Компонент Weather</div>;
 };
