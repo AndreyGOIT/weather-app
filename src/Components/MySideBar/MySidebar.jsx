f25ebc91484c10d1b0e241247327281fb395454b
AndreyGOIT <ae.borga@gmail.com>
2023-11-06 18:45:38 +0200

    sidebar input & fetch

---
+++ src/Components/MySideBar/MySidebar.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import styles from "./MySidebar.module.css";
+
+const MySidebar = () => {
+  const [city, setCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState(null);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  useEffect(() => {
+    let debounceTimeout;
+
+    const fetchData = async () => {
+      try {
+        // Отправляем запрос к OpenWeather API с использованием полученных координат
+        console.log(city);
+        console.log(apiKey);
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        setWeatherInfo(data);
+
+        console.log("newAPI Response:", data);
+        console.log("WeatherInfo:", weatherInfo);
+      } catch (error) {
+        // Обработка ошибок
+        console.error("API Request Error:", error);
+      }
+    };
+
+    // Используем debounce для задержки выполнения fetchData на 500 миллисекунд после завершения ввода
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(() => {
+      fetchData();
+    }, 500);
+
+    // Очистка таймаута при каждом изменении ввода
+    return () => {
+      clearTimeout(debounceTimeout);
+    };
+  }, [city, apiKey]);
+
+  const handleWeatherInfoClick = () => {
+    // Обработчик нажатия на окошко с информацией о погоде
+    // Здесь можно добавить логику для отображения подробных данных о погоде на главной странице сайта
+  };
+
+  return (
+    <div
+      className={
+        isSidebarOpen ? `${styles.sidebar} ${styles.open}` : styles.sidebar
+      }
+    >
+      <div className={styles.hamburgerIcon} onClick={toggleSidebar}>
+        ☰
+      </div>
+
+      <div className={styles.menu}>
+        <input
+          type="text"
+          placeholder="Введите город"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        {/* <button onClick={searchWeather}>Поиск</button> */}
+        {weatherInfo && (
+          <div className={styles.weatherInfo} onClick={handleWeatherInfoClick}>
+            Город: {weatherInfo.name}
+            <br />
+            Температура: {weatherInfo.main.temp}°C
+            <br />
+            Погодные условия: {weatherInfo.weather[0].description}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MySidebar;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,40 +1,52 @@
-import React, { useState } from "react";
-import Sidebar from "../Components/Sidebar/Sidebar";
-import WeatherCard from "../Components/WeatherCard/WeatherCard";
+// import React, { useState } from "react";
+// import Sidebar from "../Components/Sidebar/Sidebar";
+// import WeatherCard from "../Components/WeatherCard/WeatherCard";
 import Weather from "../Components/Weather/Weather";
-import { Center } from "@chakra-ui/react";
+// import { Center } from "@chakra-ui/react";
+// import SimpleSidebar from "../Components/SimpleSidebar/SimpleSidebar";
+import MySidebar from "../Components/MySideBar/MySidebar.jsx";
+import { Flex, Box } from "@chakra-ui/react";
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [cities, setCities] = useState([]); // Список городов
-  const [selectedCity, setSelectedCity] = useState(null); // Выбранный город
+  // const [isOpen, setIsOpen] = useState(false);
+  // const [cities, setCities] = useState([]); // Список городов
+  // const [selectedCity, setSelectedCity] = useState(null); // Выбранный город
 
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
+  // const toggleSidebar = () => {
+  //   setIsOpen(!isOpen);
+  // };
 
-  const handleSearch = (query) => {
-    // Здесь вы можете сделать запрос к API для поиска городов
-    // и обновить состояние `cities` с найденными городами.
-    setCities(query.cities);
-  };
+  // const handleSearch = (query) => {
+  //   // Здесь вы можете сделать запрос к API для поиска городов
+  //   // и обновить состояние `cities` с найденными городами.
+  //   setCities(query.cities);
+  // };
 
-  const handleCityClick = (city) => {
-    // Обработка клика на город в Sidebar
-    // Здесь вы можете обновить `selectedCity` или выполнить другие действия.
-    setSelectedCity(city);
-  };
+  // const handleCityClick = (city) => {
+  //   // Обработка клика на город в Sidebar
+  //   // Здесь вы можете обновить `selectedCity` или выполнить другие действия.
+  //   setSelectedCity(city);
+  // };
 
   return (
     <>
-      <Sidebar
+      <Flex justify={"space-between"} gap={2}>
+        <Box>
+          <MySidebar />
+        </Box>
+        {/* <SimpleSidebar /> */}
+        {/* <Sidebar
         isOpen={isOpen}
         onToggle={toggleSidebar}
         cities={cities}
         onCityClick={handleCityClick}
         onSearch={handleSearch}
-      />
-      <Center>{selectedCity ? <WeatherCard /> : <Weather />}</Center>
+      /> */}
+        <Box>
+          <Weather />
+        </Box>
+        {/* <Center>{selectedCity ? <WeatherCard /> : <Weather />}</Center> */}
+      </Flex>
     </>
   );
 };
